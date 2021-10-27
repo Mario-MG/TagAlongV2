0fd1ac53f58137afe0fccbd351b8ef2648fb1d85
Mario MG <mariomg11k@gmail.com>
2021-10-27 20:56:04 +0200

    Include TextField to add new keywords

--- app/src/main/java/com/hfad/tagalong/presentation/components/FlowKeywordList.kt
+++ app/src/main/java/com/hfad/tagalong/presentation/components/FlowKeywordList.kt
@@ -1,20 +1,29 @@
 package com.hfad.tagalong.presentation.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Tag
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.flowlayout.FlowRow
 
 @Composable
-fun <T> FlowKeywordList(
+fun <T> FlowKeywordList( // TODO: Make TextField and/or Keywords customizable?
     keywordObjects: List<T>,
-    onClickDeleteIcon: ((T) -> Unit)? = null
+    onClickDeleteIcon: ((T) -> Unit)? = null,
+    onAddNewKeyword: ((String) -> Unit)? = null
 ) {
+    val newKeyword = mutableStateOf("")
     Surface(
         color = Color.Black,
         modifier = Modifier
@@ -24,14 +33,35 @@ fun <T> FlowKeywordList(
             ),
         shape = MaterialTheme.shapes.medium
     ) {
-        FlowRow(
-            modifier = Modifier
-                .padding(8.dp)
-        ) {
-            for (keywordObject in keywordObjects) {
-                Keyword(
-                    keywordObject = keywordObject,
-                    onClickDeleteIcon = onClickDeleteIcon
+        Column {
+            FlowRow( // TODO: Make scrollable? ALSO: Explore nested scrolling
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                for (keywordObject in keywordObjects) {
+                    Keyword(
+                        keywordObject = keywordObject,
+                        onClickDeleteIcon = onClickDeleteIcon
+                    )
+                }
+            }
+            onAddNewKeyword?.let {
+                TextField(
+                    value = newKeyword.value,
+                    onValueChange = { newKeyword.value = it },
+                    label = { Text("Add a tag here...") }, // TODO: Make text customizable
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Done,
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            onAddNewKeyword(newKeyword.value)
+                            newKeyword.value = ""
+                        },
+                    ),
+                    leadingIcon = { Icon(Icons.Filled.Tag, contentDescription = "Add tag") }, // TODO: Make text customizable?
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
         }
