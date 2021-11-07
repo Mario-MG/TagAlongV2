b1fc93bb67abb95d56c6ba32b9b404cce131cc47
Mario MG <mariomg11k@gmail.com>
2021-11-07 20:32:10 +0100

    Minor changes

--- app/src/main/java/com/hfad/tagalong/presentation/ui/login/LoginFragment.kt
+++ app/src/main/java/com/hfad/tagalong/presentation/ui/login/LoginFragment.kt
@@ -1,19 +1,22 @@
 package com.hfad.tagalong.presentation.ui.login
 
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -21,8 +24,11 @@ import com.hfad.tagalong.R
 import com.hfad.tagalong.presentation.BUNDLE_KEY_URI
 import com.hfad.tagalong.presentation.theme.AppTheme
 import com.hfad.tagalong.presentation.ui.login.LoginEvent.ReceiveLoginCodeEvent
+import com.hfad.tagalong.util.loadPicture
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
 
@@ -45,12 +51,34 @@ class LoginFragment : Fragment() {
                         modifier = Modifier.fillMaxSize()
                     ) {
                         Button(
-                            onClick = {
-                                launchAuthUrl()
-                            },
-                            modifier = Modifier.align(Alignment.Center)
+                            onClick = ::launchAuthUrl,
+                            modifier = Modifier
+                                .align(Alignment.Center),
+                            shape = RoundedCornerShape(48.dp)
                         ) {
-                            Text(text = "Login")
+                            Row(
+                                modifier = Modifier
+                                    .padding(vertical = 16.dp, horizontal = 8.dp)
+                            ) {
+                                val spotifyIcon = loadPicture(
+                                    drawable = R.drawable.ic_spotify_icon_green
+                                ).value
+                                spotifyIcon?.let {
+                                    Icon(
+                                        bitmap = spotifyIcon.asImageBitmap(),
+                                        contentDescription = "Spotify Icon",
+                                        modifier = Modifier.size(36.dp)
+                                            .align(Alignment.CenterVertically),
+                                        tint = MaterialTheme.colors.onPrimary
+                                    )
+                                    Spacer(modifier = Modifier.size(20.dp))
+                                }
+                                Text(
+                                    text = "LOG IN WITH SPOTIFY",
+                                    style = MaterialTheme.typography.h3,
+                                    modifier = Modifier.align(Alignment.CenterVertically)
+                                )
+                            }
                         }
                     }
                 }
@@ -60,10 +88,7 @@ class LoginFragment : Fragment() {
 
     private fun launchAuthUrl() {
         val authUri = viewModel.buildAuthUri()
-        val customTabsIntent = CustomTabsIntent.Builder().build()
-        customTabsIntent.intent.flags =
-            Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_TOP // TODO: Is this necessary?
-        customTabsIntent.launchUrl(requireActivity(), authUri)
+        CustomTabsIntent.Builder().build().launchUrl(requireActivity(), authUri)
     }
 
     override fun onResume() {
