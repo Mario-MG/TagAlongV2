0f916d8a463e5c6eee5a2ce3b13e62886fa17f15
Mario MG <mariomg11k@gmail.com>
2021-11-21 12:00:58 +0100

    Add @ColumnInfo annotation to TagEntityPoko size field

--- app/src/main/java/com/hfad/tagalong/cache/model/TagEntityPoko.kt
+++ app/src/main/java/com/hfad/tagalong/cache/model/TagEntityPoko.kt
@@ -1,8 +1,14 @@
 package com.hfad.tagalong.cache.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Embedded
 
 data class TagEntityPoko (
-    @Embedded val tagEntity: TagEntity,
+
+    @Embedded
+    val tagEntity: TagEntity,
+
+    @ColumnInfo(name = TAG_SIZE)
     val size: Int
+
 )
