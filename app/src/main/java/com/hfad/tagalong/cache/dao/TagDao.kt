10b76172f221ce9d262328ea413f2bc1c666ec7a
Mario MG <mariomg11k@gmail.com>
2021-11-21 11:57:24 +0100

    Ignore TagEntity size field

--- app/src/main/java/com/hfad/tagalong/cache/dao/TagDao.kt
+++ app/src/main/java/com/hfad/tagalong/cache/dao/TagDao.kt
@@ -4,31 +4,42 @@ import androidx.room.*
 import com.hfad.tagalong.cache.model.*
 
 @Dao
-interface TagDao {
+abstract class TagDao {
 
     @Query("""
         SELECT t.$TAG_ID, t.$TAG_NAME, COUNT(cr.$TAG_ID) as $TAG_SIZE FROM $TAG_TABLE t
         LEFT JOIN $TRACK_TAG_CROSS_REF_TABLE cr ON t.$TAG_ID = cr.$TAG_ID
         GROUP BY t.$TAG_ID, t.$TAG_NAME
     """)
-    suspend fun getAll(): List<TagEntity>
+    abstract suspend fun getAll(): List<TagEntityPoko>
 
     @Query("""
-        SELECT * FROM $TAG_TABLE
-        WHERE $TAG_ID IN (
-            SELECT $TAG_ID FROM $TRACK_TAG_CROSS_REF_TABLE
-            WHERE $TRACK_ID = :trackId
-        )
+        SELECT t.$TAG_ID, t.$TAG_NAME, COUNT(cr.$TAG_ID) as $TAG_SIZE FROM $TAG_TABLE t
+        LEFT JOIN $TRACK_TAG_CROSS_REF_TABLE cr ON t.$TAG_ID = cr.$TAG_ID
+        WHERE cr.$TRACK_ID = :trackId
+        GROUP BY t.$TAG_ID, t.$TAG_NAME
     """)
-    suspend fun getTagsForTrackById(trackId: String): List<TagEntity>
+    abstract suspend fun getTagsForTrackById(trackId: String): List<TagEntityPoko>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(tag: TagEntity): Long
+    abstract suspend fun insert(tag: TagEntity): Long
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertAll(vararg tags: TagEntity): List<Long>
+    abstract suspend fun insert(vararg tags: TagEntity): List<Long>
 
     @Delete
-    suspend fun delete(vararg tag: TagEntity): Int
+    abstract suspend fun delete(vararg tags: TagEntity): Int
+
+    suspend fun insert(tag: TagEntityPoko): Long {
+        return insert(tag.tagEntity)
+    }
+
+    suspend fun insert(vararg tags: TagEntityPoko): List<Long> {
+        return insert(*tags.map(TagEntityPoko::tagEntity).toTypedArray())
+    }
+
+    suspend fun delete(vararg tags: TagEntityPoko): Int {
+        return delete(*tags.map(TagEntityPoko::tagEntity).toTypedArray())
+    }
 
 }
--- app/src/main/java/com/hfad/tagalong/cache/model/TagEntityMapper.kt
+++ app/src/main/java/com/hfad/tagalong/cache/model/TagEntityMapper.kt
@@ -3,14 +3,24 @@ package com.hfad.tagalong.cache.model
 import com.hfad.tagalong.domain.model.Tag
 import com.hfad.tagalong.domain.util.DomainMapper
 
-class TagEntityMapper : DomainMapper<TagEntity, Tag> {
+class TagEntityMapper : DomainMapper<TagEntityPoko, Tag> {
 
-    override fun mapToDomainModel(model: TagEntity): Tag {
-        return Tag(id = model.id, name = model.name, size = model.size)
+    override fun mapToDomainModel(model: TagEntityPoko): Tag {
+        return Tag(
+            id = model.tagEntity.id,
+            name = model.tagEntity.name,
+            size = model.size
+        )
     }
 
-    override fun mapFromDomainModel(domainModel: Tag): TagEntity {
-        return TagEntity(id = domainModel.id, name = domainModel.name, size = domainModel.size)
+    override fun mapFromDomainModel(domainModel: Tag): TagEntityPoko {
+        return TagEntityPoko(
+            tagEntity = TagEntity(
+                id = domainModel.id,
+                name = domainModel.name
+            ),
+            size = domainModel.size
+        )
     }
 
 }
