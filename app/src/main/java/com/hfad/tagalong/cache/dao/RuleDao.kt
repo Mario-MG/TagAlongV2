0b0613a0a1b5ef96db30792e04e87f7bd66ed190
Mario MG <mariomg11k@gmail.com>
2021-12-07 17:18:59 +0100

    Apply rules when tags are added to a track

--- app/src/main/java/com/hfad/tagalong/cache/dao/RuleDao.kt
+++ app/src/main/java/com/hfad/tagalong/cache/dao/RuleDao.kt
@@ -10,18 +10,25 @@ abstract class RuleDao {
     @Query("SELECT * FROM $RULE_TABLE")
     abstract suspend fun getAll(): List<RulePoko>
 
+    suspend fun getRulesFulfilledByTagIds(newTagId: Long, vararg originalTagsIds: Long): List<RulePoko> {
+        return if (originalTagsIds.isNotEmpty()) {
+            _getRulesFulfilledByTagIds(newTagId = newTagId, originalTagsIds = originalTagsIds)
+        } else {
+            _getRulesFulfilledByTagIds(newTagId = newTagId)
+        }
+    }
+
     @Transaction
     @Query("""
         SELECT r.* FROM $RULE_TABLE r
         JOIN $RULE_TAG_CROSS_REF_TABLE rt ON r.$RULE_ID = rt.$RULE_ID
         WHERE r.$RULE_AUTOUPDATE = 1
+                AND rt.$TAG_ID = :newTagId
         AND ((
                 r.$RULE_OPTIONALITY = 1
-                AND rt.$TAG_ID = :newTagId
-                AND rt.$TAG_ID NOT IN (:originalTagsIds) -- TODO: Comprobar que esto funciona
+                AND rt.$TAG_ID NOT IN (:originalTagsIds)
             ) OR (
                 r.$RULE_OPTIONALITY = 0
-                AND rt.$TAG_ID = :newTagId -- TODO: Comprobar que esto funciona (en caso afirmativo, refactorizar fuera del OR)
                 AND rt.$RULE_ID NOT IN (
                     SELECT DISTINCT rt.$RULE_ID FROM $RULE_TAG_CROSS_REF_TABLE rt
                     WHERE rt.$TAG_ID NOT IN (:newTagId, :originalTagsIds)
@@ -29,7 +36,26 @@ abstract class RuleDao {
             )
         )
     """)
-    abstract suspend fun getRulesFulfilledByTagIds(newTagId: Long, vararg originalTagsIds: Long): List<RulePoko>
+    internal abstract suspend fun _getRulesFulfilledByTagIds(newTagId: Long, vararg originalTagsIds: Long): List<RulePoko>
+
+    @Transaction
+    @Query("""
+        SELECT r.* FROM $RULE_TABLE r
+        JOIN $RULE_TAG_CROSS_REF_TABLE rt ON r.$RULE_ID = rt.$RULE_ID
+        WHERE r.$RULE_AUTOUPDATE = 1
+                AND rt.$TAG_ID = :newTagId
+        AND ((
+                r.$RULE_OPTIONALITY = 1
+            ) OR (
+                r.$RULE_OPTIONALITY = 0
+                AND rt.$RULE_ID NOT IN (
+                    SELECT DISTINCT rt.$RULE_ID FROM $RULE_TAG_CROSS_REF_TABLE rt
+                    WHERE rt.$TAG_ID NOT IN (:newTagId)
+                )
+            )
+        )
+    """)
+    internal abstract suspend fun _getRulesFulfilledByTagIds(newTagId: Long): List<RulePoko>
 
     suspend fun insert(rulePoko: RulePoko) {
         val ruleId = _insert(rulePoko.rule)
@@ -47,7 +73,7 @@ abstract class RuleDao {
         }.toTypedArray())
     }
 
-    @Insert(onConflict = OnConflictStrategy.ABORT)
+    @Insert
     internal abstract suspend fun _insert(rule: RuleEntity): Long
 
     @Insert
