f09f6a095c75c83a32b143f17ba8519a8dd7285c
Mario MG <mariomg11k@gmail.com>
2022-01-09 17:26:08 +0100

    Remove unused import

--- app/src/main/java/com/hfad/tagalong/network/TokenAuthenticator.kt
+++ app/src/main/java/com/hfad/tagalong/network/TokenAuthenticator.kt
@@ -8,7 +8,6 @@ import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
-import retrofit2.HttpException
 import java.io.IOException
 
 // Source: https://www.simplifiedcoding.net/retrofit-authenticator-refresh-token/
