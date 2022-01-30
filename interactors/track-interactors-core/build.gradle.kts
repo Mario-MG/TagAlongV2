9b0902baf973e21f58bdb871532b366d61fcc802
Mario MG <mariomg11k@gmail.com>
2022-01-30 20:46:21 +0100

    Modularize Track interactors V

--- interactors/track-interactors-core/build.gradle.kts
+++ interactors/track-interactors-core/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     "implementation"(project(":domain:tag-domain"))
     "implementation"(project(":domain:track-domain"))
     "implementation"(project(":domain:playlist-domain"))
+    "implementation"(project(":domain:rule-domain"))
 }
