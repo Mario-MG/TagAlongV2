9a0a29a8652b9660749e1a33f6bd3fdfad7753d2
Mario MG <mariomg11k@gmail.com>
2022-02-01 20:03:57 +0100

    Modularize Playlist interactors II

--- interactors/playlist-interactors-core/src/main/java/com/hfad/tagalong/playlist_interactors_core/repositories/PlaylistNetworkRepository.kt
+++ interactors/playlist-interactors-core/src/main/java/com/hfad/tagalong/playlist_interactors_core/repositories/PlaylistNetworkRepository.kt
@@ -5,6 +5,8 @@ import com.hfad.tagalong.track_domain.Track
 
 interface PlaylistNetworkRepository {
 
+    suspend fun getPlaylists(offset: Int, limit: Int): List<Playlist>
+
     suspend fun addTracksToPlaylist(tracks: List<Track>, playlist: Playlist)
 
 }
