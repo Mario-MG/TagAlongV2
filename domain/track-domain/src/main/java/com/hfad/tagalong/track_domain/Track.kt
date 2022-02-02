7696ae5074c964cfa20173cfdee45674414fb2f1
Mario MG <mariomg11k@gmail.com>
2022-02-02 20:05:56 +0100

    Refactor Track members into TrackInfo

--- domain/track-domain/src/main/java/com/hfad/tagalong/track_domain/Track.kt
+++ domain/track-domain/src/main/java/com/hfad/tagalong/track_domain/Track.kt
@@ -2,9 +2,38 @@ package com.hfad.tagalong.track_domain
 
 data class Track(
     val id: String,
-    val name: String,
-    val album: String,
-    val artists: List<String>,
-    val imageUrl: String?,
+    val info: TrackInfo
+) {
+    constructor(
+        id: String,
+        name: String,
+        album: String,
+        artists: List<String>,
+        imageUrl: String?,
+        uri: String
+    ) : this(
+        id = id,
+        info = TrackInfo(
+            name = name,
+            album = album,
+            artists = artists,
+            imageUrl = imageUrl,
+            uri = uri
+        )
+    )
+
+    val name: String
+        get() = this.info.name
+
+    val album: String
+        get() = this.info.album
+
+    val artists: List<String>
+        get() = this.info.artists
+
+    val imageUrl: String?
+        get() = this.info.imageUrl
+
     val uri: String
-)
+        get() = this.info.uri
+}
