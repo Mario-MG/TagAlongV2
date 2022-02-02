70d3e2a3dc6ff3a838e61303f4d949ef84dd3ccb
Mario MG <mariomg11k@gmail.com>
2022-02-02 19:42:12 +0100

    Refactor Rule members into RuleInfo + Modularize Rule interactors III

--- interactors/rule-interactors-core/src/main/java/com/hfad/tagalong/rule_interactors_core/repositories/RuleCacheRepository.kt
+++ interactors/rule-interactors-core/src/main/java/com/hfad/tagalong/rule_interactors_core/repositories/RuleCacheRepository.kt
@@ -1,6 +1,7 @@
 package com.hfad.tagalong.rule_interactors_core.repositories
 
 import com.hfad.tagalong.rule_domain.Rule
+import com.hfad.tagalong.rule_domain.RuleInfo
 import com.hfad.tagalong.tag_domain.Tag
 
 interface RuleCacheRepository {
@@ -8,5 +9,7 @@ interface RuleCacheRepository {
     suspend fun getAll(): List<Rule>
 
     suspend fun getRulesForTags(newTag: Tag, originalTags: List<Tag>): List<Rule>
+
+    suspend fun create(ruleInfo: RuleInfo): Rule
 
 }
