fea095ca5e5930fe65c31a5ad14d04745b2d19c6
Mario MG <mariomg11k@gmail.com>
2022-02-06 20:11:46 +0100

    Refactor TrackTagCrossRefMapper to delegate id extraction to the appropiate mappers

--- app/src/main/java/com/hfad/tagalong/cache/model/TrackTagCrossRefMapper.kt
+++ app/src/main/java/com/hfad/tagalong/cache/model/TrackTagCrossRefMapper.kt
@@ -3,9 +3,15 @@ package com.hfad.tagalong.cache.model
 import com.hfad.tagalong.tag_domain.Tag
 import com.hfad.tagalong.track_domain.Track
 
-class TrackTagCrossRefMapper {
+class TrackTagCrossRefMapper(
+    private val trackEntityMapper: TrackEntityMapper,
+    private val tagEntityMapper: TagEntityMapper
+) {
 
     fun mapFromDomainModel(track: Track, tag: Tag): TrackTagCrossRef =
-        TrackTagCrossRef(trackId = track.uri, tagId = tag.id)
+        TrackTagCrossRef(
+            trackId = trackEntityMapper.mapFromDomainModel(track).id,
+            tagId = tagEntityMapper.mapFromDomainModel(tag).tagEntity.id
+        )
 
 }
